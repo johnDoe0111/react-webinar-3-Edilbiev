0d6b109c3b45ef4905b9228150ee2e07ab36e4a2
johnDoe0111 <johndoe011@mail.ru>
2023-11-29 16:10:18 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import {createElement} from './utils.js';
-import App from './app.js';
-import Store from './store.js';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app.js";
+import Store from "./store.js";
 
 const store = new Store({
   list: [
-    {code: 1, title: 'Название элемента', views: 0},
-    {code: 2, title: 'Некий объект', views: 0},
-    {code: 3, title: 'Заголовок', views: 0},
-    {code: 4, title: 'Очень длинное название элемента из семи слов', views: 0},
-    {code: 5, title: 'Запись', views: 0},
-    {code: 6, title: 'Шестая запись', views: 0},
-    {code: 7, title: 'Седьмая запись', views: 0},
-  ]
+    { code: 1, title: "Название элемента", views: 0 },
+    { code: 2, title: "Некий объект", views: 0 },
+    { code: 3, title: "Заголовок", views: 0 },
+    {
+      code: 4,
+      title: "Очень длинное название элемента из семи слов",
+      views: 0,
+    },
+    { code: 5, title: "Запись", views: 0 },
+    { code: 6, title: "Шестая запись", views: 0 },
+    { code: 7, title: "Седьмая запись", views: 0 },
+  ],
 });
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 store.subscribe(() => {
-  root.render(<App store={store}/>);
+  root.render(<App store={store} />);
 });
 
 // Первый рендер приложения
-root.render(<App store={store}/>);
+root.render(<App store={store} />);
